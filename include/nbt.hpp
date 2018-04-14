6b2795639bc2a76a9e5a1ea91737cec3069e63e5
Alexandre Szymocha <szymoc_a@epitech.eu>
2018-04-14 16:17:00 +0200

    better data model + parsing char and byte

--- include/nbt.hpp
+++ include/nbt.hpp
@@ -41,14 +41,32 @@ namespace detail {
 template <class _List>
 struct integer_list_hash
 {
-	std::size_t operator()(_List const &l) const noexcept
+	std::size_t
+	operator()(_List const &l) const noexcept
 	{
-		std::size_t seed = std::distance(l.cbegin(), l.cend());
-		for (auto const &i : l) {
-			seed ^= i + 0x9e3779b9 + (seed << 6) + (seed >> 2);
-		}
-		return seed;
+		std::size_t _Seed = std::distance(l.cbegin(), l.cend());
+		for (auto const &_I : l)
+			_Seed ^= _I + 0x9e3779b9
+				+ (_Seed << 6) + (_Seed >> 2);
+		return _Seed;
 	}
+};
+
+template <typename _Char>
+static inline std::int_least16_t
+b2tos16(_Char const *buf)
+{
+#if '\x11\x22\x33\x44' == 0x11'22'33'44
+	return (buf[0] << 8) | buf[1];
+#else
+	return (buf[1] << 8) | buf[0];
+#endif
+}
+
+enum class state
+{	S
+,	S1, S2, S3, S4, S5, S6, S7, S8, S9, S10, S11, S12
+,	F
 };
 
 } // detail
@@ -77,20 +95,20 @@ template
 	,	_Allocator<char>
 	>
 ,	typename _List_type = _List
-	<	std::unique_ptr<void, void (*) (void const *)>
-	,	_Allocator<std::unique_ptr<void, void (*) (void const *)>>
+	<	std::unique_ptr<void, void (*) (void *)>
+	,	_Allocator<std::unique_ptr<void, void (*) (void *)>>
 	>
 ,	typename _Compound_type = _Compound
 	<	_String_type
-	,	std::unique_ptr<void, void (*) (void const *)>
+	,	std::unique_ptr<void, void (*) (void *)>
 	,	detail::integer_list_hash<_String_type>
 	,	std::equal_to<_String_type>
-	,	_Allocator<std::unique_ptr<void, void (*) (void const *)>>
+	,	_Allocator<std::unique_ptr<void, void (*) (void *)>>
 	>
 ,	typename _Int_array_type = _Int_array<_Int, _Allocator<_Int>>
 ,	typename _Long_array_type = _Long_array<_Long, _Allocator<_Long>>
 ,	typename _Node = std::variant
-	<	_Char, _Byte, _Short, _Int, _Long, _Float, _Double
+	<	_Byte, _Short, _Int, _Long, _Float, _Double
 	,	_Byte_array_type, _String_type, _List_type, _Compound_type
 	,	_Int_array_type, _Long_array_type
 	>
@@ -98,13 +116,61 @@ template
 std::unique_ptr<_Node>
 parse
 (	std::basic_istream<_In_char> &&in
-//,	_Allocator const & __a = _Allocator()
+,	_Allocator<_Node> __a = _Allocator<_Node>()
 )
 {
-	(void)(in);
-	return std::unique_ptr<_Node>();
-}
+	using _Node_ptr = std::unique_ptr<_Node>;
+	using _A = typename std::allocator_traits<_Allocator<_Node>>;
+	using _In_traits = typename std::decay_t<decltype(in)>::traits_type;
+	static constexpr typename _In_traits::int_type _Tag_byt{0x01};
+	static constexpr typename _In_traits::int_type _Tag_sht{0x02};
+	std::unordered_map
+	<	detail::state
+	,	std::unordered_map<typename _In_traits::int_type, int>
+	> const trans =
+	{	{ detail::state::F, {{ _In_traits::eof(), '0' }} }
+	,	{	detail::state::S
+		,	{	{ _Tag_byt, '1' }
+			,	{ _Tag_sht, '2' }
+			}
+		}
+	};
 
+	std::stack<_Node_ptr> ret;
+	std::stack<detail::state> ss;
+	ss.push(detail::state::F);
+	ss.push(detail::state::S);
+
+loop:
+	switch (trans.at(ss.top()).at(in.peek()))
+	{
+	case '0':
+		goto end;
+	case '1':
+		{
+			ss.pop();
+			in.get();
+			_Node *tmp = _A::allocate(__a, 1);
+			_A::construct(__a, tmp, (_Byte)(in.get()));
+			ret.push(_Node_ptr(tmp));
+			break;
+		}
+	case '2':
+		{
+			ss.pop();
+			in.get();
+			_Node *tmp = _A::allocate(__a, 1);
+			_In_char buf[2];
+			in.read(buf, 2U);
+			_A::construct(__a, tmp, (_Short)(detail::b2tos16(buf)));
+			ret.push(_Node_ptr(tmp));
+			break;
+		}
+	}
+	goto loop;
+end:
+	return std::move(ret.top());
+}
 
 } // nbt
 
