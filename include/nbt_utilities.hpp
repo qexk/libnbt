e18d3d6efeaff135edee81cc7ea67886de3f295c
Alexandre Szymocha <szymoc_a@epitech.eu>
2018-05-08 15:09:28 +0200

    added special case for TAG_Compound in TAG_List

--- include/nbt_utilities.hpp
+++ include/nbt_utilities.hpp
@@ -81,302 +81,6 @@ NBT_GETTER__BUILDER_(long_array, 11);
 
 namespace detail {
 
-template <typename _Default, bool, unsigned, class>
-struct _Variant_alternative_or_default
-{
-	static inline constexpr auto is_default = true;
-	using type = _Default;
-};
-
-template <typename _Default, unsigned _Alt, class _Variant>
-struct _Variant_alternative_or_default
-<	_Default
-,	true
-,	_Alt
-,	_Variant
->
-{
-	static inline constexpr auto is_default = false;
-	using type = std::variant_alternative_t<_Alt, _Variant>;
-};
-
-template <typename _Default, unsigned _Alt, class _Variant>
-using variant_alternative_or_default =
-	_Variant_alternative_or_default
-	<	_Default
-	,	_Alt < std::variant_size_v<_Variant>
-	,	_Alt
-	,	_Variant
-	>;
-
-template
-<	unsigned _Alt
-,	typename _Variant
-,	typename _List
->
-class list_wrapper
-{
-# ifdef NBT_UNIT_TEST_
-public:
-# endif // NBT_UNIT_TEST_
-	static inline _List const defaulted = _List();
-	_List const *cont;
-public:
-	using variant_or_default = variant_alternative_or_default
-	<	_Variant
-	,	_Alt, _Variant
-	>;
-	using value_type =
-		std::add_const_t
-		<	std::conditional_t
-			<	_Alt == 8
-			,	std::unique_ptr
-				<	_Variant
-				,	void (*) (_Variant *)
-				>
-			,	typename variant_or_default::type
-			>
-		>;
-	using reference = value_type &;
-	using const_reference = value_type const &;
-	using difference_type = std::ptrdiff_t;
-	using size_type = std::size_t;
-
-	class iterator
-	{
-# ifdef NBT_UNIT_TEST_
-	public:
-# endif // NBT_UNIT_TEST_
-		typename _List::const_iterator it;
-	public:
-		using difference_type = std::ptrdiff_t;
-		using value_type = list_wrapper<_Alt, _Variant, _List>
-					::value_type;
-		using pointer = value_type *;
-		using reference = value_type &;
-		using iterator_category = std::input_iterator_tag;
-
-		iterator()
-		:	it()
-		{}
-
-		iterator(typename _List::const_iterator const &it)
-		:	it(it)
-		{}
-
-		inline iterator &
-		operator++(void)
-		{
-			++it;
-			return *this;
-		}
-
-		inline iterator
-		operator++(int)
-		{
-			iterator ret(*this);
-			this->operator++();
-			return ret;
-		}
-
-		std::conditional_t
-		<	_Alt == 8
-		,	_Variant *
-		,	pointer
-		>
-		operator->(void) const
-		{
-			auto const variant = reinterpret_cast<_Variant *>
-			(	it->get()
-			);
-			if constexpr (_Alt == 8)
-				return variant;
-			if constexpr (variant_or_default::is_default)
-				return variant;
-			else
-				return std::get_if<_Alt>(variant);
-		}
-
-		inline std::conditional_t
-		<	_Alt == 8
-		,	_Variant *
-		,	reference
-		>
-		operator*(void) const
-		{
-			if constexpr (_Alt == 8)
-				return reinterpret_cast<_Variant *>
-				(	it->get()
-				);
-			else
-				return *this->operator->();
-		}
-
-		inline bool
-		operator==(iterator const &rhs) const
-		{
-			return this->it == rhs.it;
-		}
-
-		inline bool
-		operator!=(iterator const &rhs) const
-		{
-			return this->it != rhs.it;
-		}
-	};
-	using const_iterator = iterator const;
-
-	list_wrapper()
-	:	cont{&list_wrapper<_Alt, _Variant, _List>::defaulted}
-	{}
-
-	list_wrapper(_List const *cont)
-	:	cont{cont}
-	{}
-
-	list_wrapper(list_wrapper const &a)
-	:	cont{a.cont}
-	{}
-
-	~list_wrapper()
-	{}
-
-	list_wrapper &
-	operator=(list_wrapper const &a)
-	{
-		this->cont = a.cont;
-		return *this;
-	}
-
-	inline const_iterator
-	begin(void) const
-	{
-		return iterator(this->cont->cbegin());
-	}
-
-	inline const_iterator
-	end(void) const
-	{
-		return iterator(this->cont->cend());
-	}
-
-	inline const_iterator
-	cbegin(void) const
-	{
-		return this->begin();
-	}
-
-	inline const_iterator
-	cend(void) const
-	{
-		return this->end();
-	}
-
-	template
-	<	unsigned _Alt_a
-	,	typename _Variant_a
-	,	typename _List_a
-	>
-	bool
-	operator==(list_wrapper<_Alt_a, _Variant_a, _List_a> const &a) const
-	{
-		return
-			this->cont == a.cont
-			|| std::equal
-			(	this->cbegin(), this->cend()
-			,	a.cbegin(), a.cend()
-			);
-	}
-
-	template <class _Container>
-	bool
-	operator==(_Container const &c) const
-	{
-		return	std::equal
-		(	this->cbegin(), this->cend()
-		,	c.cbegin(), c.cend()
-		);
-	}
-
-	template
-	<	unsigned _Alt_a
-	,	typename _Variant_a
-	,	typename _List_a
-	>
-	inline bool
-	operator!=(list_wrapper<_Alt_a, _Variant_a, _List_a> const &a) const
-	{
-		return !this->operator==(a);
-	}
-
-	void
-	swap(list_wrapper &a) noexcept
-	{
-		auto const tmp = this->cont;
-		this->cont = a.cont;
-		a.cont = tmp;
-	}
-
-	void
-	swap(list_wrapper &&a) noexcept
-	{
-		this->operator=(std::forward(a));
-	}
-
-	friend void
-	swap(list_wrapper &a, list_wrapper &b) noexcept
-	{
-		return a.swap(b);
-	}
-
-	inline size_type
-	size(void) const
-	{
-		return this->cont->size();
-	}
-	
-	inline size_type
-	max_size(void) const
-	{
-		return this->cont->max_size();
-	}
-
-	inline bool
-	empty(void) const
-	{
-		return this->cont->empty();
-	}
-
-	inline const_reference
-	front(void) const
-	{
-		return *this->begin();
-	}
-
-	inline const_reference
-	back(void) const
-	{
-		return *iterator(--this->cont->cend());
-	}
-
-	inline const_reference
-	operator[](size_type const n) const
-	{
-		return *iterator(this->cont->cbegin() + n);
-	}
-
-	const_reference
-	at(size_type const n) const
-	{
-		if (n >= this->size())
-			throw std::out_of_range
-			(	"nbt::detail::list_wrapper"
-			);
-		else
-			return this->operator[](n);
-	}
-};
-
 template
 <	typename _Variant
 ,	typename _Compound
@@ -630,6 +334,341 @@ public:
 			throw std::out_of_range
 			(	"nbt::detail::compound_wrapper"
 			);
+	}
+};
+
+template <typename _Default, bool, unsigned, class>
+struct _Variant_alternative_or_default
+{
+	static inline constexpr auto is_default = true;
+	using type = _Default;
+};
+
+template <typename _Default, unsigned _Alt, class _Variant>
+struct _Variant_alternative_or_default
+<	_Default
+,	true
+,	_Alt
+,	_Variant
+>
+{
+	static inline constexpr auto is_default = false;
+	using type = std::variant_alternative_t<_Alt, _Variant>;
+};
+
+template <typename _Default, unsigned _Alt, class _Variant>
+using variant_alternative_or_default =
+	_Variant_alternative_or_default
+	<	_Default
+	,	_Alt < std::variant_size_v<_Variant>
+	,	_Alt
+	,	_Variant
+	>;
+
+template <typename _Default, unsigned _Alt, class _Variant>
+using variant_alternative_or_default_t =
+	typename
+		variant_alternative_or_default<_Default, _Alt, _Variant>
+			::type;
+
+template
+<	unsigned _Alt
+,	typename _Variant
+,	typename _List
+>
+class list_wrapper
+{
+# ifdef NBT_UNIT_TEST_
+public:
+# endif // NBT_UNIT_TEST_
+	static inline _List const defaulted = _List();
+	_List const *cont;
+public:
+	using variant_or_default = variant_alternative_or_default
+	<	_Variant
+	,	_Alt, _Variant
+	>;
+	using value_type =
+		std::conditional_t
+		<	_Alt == 8
+		,	_Variant *
+		,	std::conditional_t
+			<	_Alt == 9
+			,	compound_wrapper
+				<	_Variant
+				,	typename variant_or_default::type
+				,	variant_alternative_or_default_t
+					<	std::string
+					,	nbt::string, _Variant
+					>
+				>
+			,	typename variant_or_default::type
+			>
+		> const;
+	using reference = value_type &;
+	using const_reference =
+		std::conditional_t
+		<	_Alt == 9
+		,	value_type const
+		,	value_type const &
+		>;
+	using difference_type = std::ptrdiff_t;
+	using size_type = std::size_t;
+
+	class iterator
+	{
+# ifdef NBT_UNIT_TEST_
+	public:
+# endif // NBT_UNIT_TEST_
+		typename _List::const_iterator it;
+	public:
+		using difference_type = std::ptrdiff_t;
+		using value_type = list_wrapper<_Alt, _Variant, _List>
+					::value_type;
+		using pointer = value_type *;
+		using reference = value_type &;
+		using iterator_category = std::input_iterator_tag;
+
+		iterator()
+		:	it()
+		{}
+
+		iterator(typename _List::const_iterator const &it)
+		:	it(it)
+		{}
+
+		inline iterator &
+		operator++(void)
+		{
+			++it;
+			return *this;
+		}
+
+		inline iterator
+		operator++(int)
+		{
+			iterator ret(*this);
+			this->operator++();
+			return ret;
+		}
+
+		std::conditional_t
+		<	_Alt == 8
+		,	_Variant *
+		,	std::conditional_t
+			<	_Alt == 9
+			,	std::unique_ptr<value_type>
+			,	pointer
+			>
+		>
+		operator->(void) const
+		{
+			auto const variant = reinterpret_cast<_Variant *>
+			(	it->get()
+			);
+			if constexpr (_Alt == 8)
+				return variant;
+			else if constexpr (_Alt == 9)
+				return std::unique_ptr<value_type>
+				(	value_type
+					(	std::get_if<_Alt>(variant)
+					)
+				);
+			else if constexpr (variant_or_default::is_default)
+				return variant;
+			else
+				return std::get_if<_Alt>(variant);
+		}
+
+		inline std::conditional_t
+		<	_Alt == 8
+		,	_Variant *
+		,	std::conditional_t
+			<	_Alt == 9
+			,	value_type
+			,	reference
+			>
+		>
+		operator*(void) const
+		{
+			if constexpr (_Alt == 8)
+				return reinterpret_cast<_Variant *>
+				(	it->get()
+				);
+			else if constexpr (_Alt == 9)
+				return value_type
+				(	std::get_if<_Alt>
+					(	reinterpret_cast<_Variant *>
+						(	it->get()
+						)
+					)
+				);
+			else
+				return *this->operator->();
+		}
+
+		inline bool
+		operator==(iterator const &rhs) const
+		{
+			return this->it == rhs.it;
+		}
+
+		inline bool
+		operator!=(iterator const &rhs) const
+		{
+			return this->it != rhs.it;
+		}
+	};
+	using const_iterator = iterator const;
+
+	list_wrapper()
+	:	cont{&list_wrapper<_Alt, _Variant, _List>::defaulted}
+	{}
+
+	list_wrapper(_List const *cont)
+	:	cont{cont}
+	{}
+
+	list_wrapper(list_wrapper const &a)
+	:	cont{a.cont}
+	{}
+
+	~list_wrapper()
+	{}
+
+	list_wrapper &
+	operator=(list_wrapper const &a)
+	{
+		this->cont = a.cont;
+		return *this;
+	}
+
+	inline const_iterator
+	begin(void) const
+	{
+		return iterator(this->cont->cbegin());
+	}
+
+	inline const_iterator
+	end(void) const
+	{
+		return iterator(this->cont->cend());
+	}
+
+	inline const_iterator
+	cbegin(void) const
+	{
+		return this->begin();
+	}
+
+	inline const_iterator
+	cend(void) const
+	{
+		return this->end();
+	}
+
+	template
+	<	unsigned _Alt_a
+	,	typename _Variant_a
+	,	typename _List_a
+	>
+	bool
+	operator==(list_wrapper<_Alt_a, _Variant_a, _List_a> const &a) const
+	{
+		return
+			this->cont == a.cont
+			|| std::equal
+			(	this->cbegin(), this->cend()
+			,	a.cbegin(), a.cend()
+			);
+	}
+
+	template <class _Container>
+	bool
+	operator==(_Container const &c) const
+	{
+		return	std::equal
+		(	this->cbegin(), this->cend()
+		,	c.cbegin(), c.cend()
+		);
+	}
+
+	template
+	<	unsigned _Alt_a
+	,	typename _Variant_a
+	,	typename _List_a
+	>
+	inline bool
+	operator!=(list_wrapper<_Alt_a, _Variant_a, _List_a> const &a) const
+	{
+		return !this->operator==(a);
+	}
+
+	void
+	swap(list_wrapper &a) noexcept
+	{
+		auto const tmp = this->cont;
+		this->cont = a.cont;
+		a.cont = tmp;
+	}
+
+	void
+	swap(list_wrapper &&a) noexcept
+	{
+		this->operator=(std::forward(a));
+	}
+
+	friend void
+	swap(list_wrapper &a, list_wrapper &b) noexcept
+	{
+		return a.swap(b);
+	}
+
+	inline size_type
+	size(void) const
+	{
+		return this->cont->size();
+	}
+	
+	inline size_type
+	max_size(void) const
+	{
+		return this->cont->max_size();
+	}
+
+	inline bool
+	empty(void) const
+	{
+		return this->cont->empty();
+	}
+
+	inline const_reference
+	front(void) const
+	{
+		return *this->begin();
+	}
+
+	inline const_reference
+	back(void) const
+	{
+		return *iterator(--this->cont->cend());
+	}
+
+	inline const_reference
+	operator[](size_type const n) const
+	{
+		return *iterator(this->cont->cbegin() + n);
+	}
+
+	const_reference
+	at(size_type const n) const
+	{
+		if (n >= this->size())
+			throw std::out_of_range
+			(	"nbt::detail::list_wrapper"
+			);
+		else
+			return this->operator[](n);
 	}
 };
 
